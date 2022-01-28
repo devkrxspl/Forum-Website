5a3cbef193135bf7f965ca93883c9aca4e33effd
Krxspl <69094742+devkrxspl@users.noreply.github.com>
2022-01-28 02:12:24 +0000

    Implemented logging in.

--- Socket/loginhandler.js
+++ Socket/loginhandler.js
@@ -19,13 +19,30 @@ function invoke(socket) {
     // Checking if username and password are not null
     if (username && password) {
       
-      logHandler.log("Authentication", `Login attempt made. (\\timestamp\\)\nUsername: ${username}\nPassword: ${password}`);
+      logHandler.log("Authentication", `Login attempt made by user '${username}' (\\timestamp\\)`);
 
       // DB needs async calls
       (async () => {
-
+        
+        // Checking if username exists
         if (await logindb.contains(username)) {
+          
+          // Getting user credentials 
+          const userCredentials = (await logindb.raw())[username];
+          const hashedPassword = userCredentials.password;
+          const salt = userCredentials.salt; 
 
+          // Hashing given password
+          const userHashedPassword = await bcrypt.hash(password, salt);
+          
+          // Checking if the user's hashed password matches the stored one
+          if (userHashedPassword == hashedPassword) {
+            
+            logHandler.log("Authentication", `Login attempt by user '${username}' successful. (\\timestamp\\)`);
+          } else {
+
+            logHandler.log("Authentication", `Login attempt by user '${username}' unsuccessful. (\\timestamp\\)`);
+          }
         } else {
           logHandler.log("Authentication", `Invalid username '${username}' provided during login. (\\timestamp\\)`);
         }
