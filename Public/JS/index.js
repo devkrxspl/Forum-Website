f7da0d941c4fa5ed8e68ca42197be67909ec0a40
Krxspl <69094742+devkrxspl@users.noreply.github.com>
2022-03-03 22:13:29 +0000

    Finished rough home page styling

--- Public/JS/index.js
+++ Public/JS/index.js
@@ -1,6 +1,11 @@
 // Constants 
 const socket = io();
 
+const resize_events = [
+  "onresize",
+  "onload"
+];
+
 // Functions
 function logout() {
   delete localStorage.jwt;
@@ -8,17 +13,33 @@ function logout() {
 }
 
 // Main 
+resize_events.forEach(function(e) {
+  window[e] = function(event) {
+    const width = window.innerWidth|| document.documentElement.clientWidth || document.body.clientWidth;
+  
+    if (width < 1337) {
+      document.getElementById("menu-bar-left").style.opacity = "0";
+      document.getElementById("menu-bar-right").style.opacity = "0";
+  
+      document.getElementById("post-container").style.right = "calc(50% - 350px)";
+    } else {
+      document.getElementById("menu-bar-left").style.opacity = "1";
+      document.getElementById("menu-bar-right").style.opacity = "1";
+  
+      document.getElementById("post-container").style.right = "max(80px + 300px, calc(50% - 350px))";
+    }
+  }
+});
+
 (async () => {
   if (localStorage.jwt != undefined) {
     
     const response = await sendMessage("getUserInfo", {jwt : localStorage.jwt}, 5000);
 
     if (!response.error) {
-      document.getElementById("welcomeMessage").innerHTML = `Welcome, ${response.username}`;
+      
     } else if (response.error == 4) {
-
       delete localStorage.jwt;
-      document.location = "https://Forum-Website.devkrxspl.repl.co";
     } else if (response.error == 5) {
       alert("Error connecting to server.");
     } else {
